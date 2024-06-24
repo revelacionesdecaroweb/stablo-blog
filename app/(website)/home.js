8180948063a0150f30eeef05a732fff913c2a62d
unknown <revelacionesdecaroweb@gmail.com>
2024-06-24 11:19:58 -0600

    prove changes to metatag

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Container from "@/components/container";
 
 import ArchivePage from "./archive/page";
-import Head from 'next/head'; 
+
 
 export default  function Post() {
 
@@ -11,15 +11,10 @@ export default  function Post() {
 
   return (
 
- 
+
     <>
 
-   <head>
-      <meta name="title" conten= 'Revelaciones de Caro'/>
-      <meta name="description" conten= 'Blog de Astrología y Filosofía'/>
-    </head>
-
-     
+     <ArchivePage></ArchivePage>
         
    
 
